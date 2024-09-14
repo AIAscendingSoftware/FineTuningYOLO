9f982281bedaa46891bc7eb4dd4d85b0fe043e1c
AI@AscendingSoftware <pranith@ascendingsoftware.com>
2024-09-14 10:52:00 +0530

    data preparation has done, dataset validatiion automation code has written and tested

---
+++ preprocessing/.py
@@ -0,0 +1,5 @@
+from math import ceil
+
+
+train_count =ceil(25)
+print('train_count:',train_count,type(train_count))
--- train_yolo.py
+++ train_yolo.py
@@ -4,7 +4,15 @@ from ultralytics import YOLO
 model = YOLO('yolov8x.pt')  # Using the yolov8x model
 
 # Fine-tune the model on the custom dataset
-model.train(data='E:\AI Ascending Software\AS AI Projects\AI PackageGuard\AI-PackageGuard\yolo\yolo_objects_detection\fineTuneYOLO\throwing_parcel.yaml', epochs=50, imgsz=640, batch=16)
-
+model.train(data='E:\AI Ascending Software\AS AI Projects\AI PackageGuard\AI-PackageGuard\yolo\yolo_objects_detection\fineTuneYOLO\throwing_parcel.yaml', 
+            epochs=50, 
+            imgsz=640, 
+            batch=16,
+            name='yolov8_finetuned'       # Run name for storing logs and weights
+)
+# After training, the model weights will be saved in 'runs/train/yolov8_finetuned'
 '''This script will automatically save the 
 best model weights (best.pt) after training.'''
+
+
+
